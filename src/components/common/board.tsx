67f41e08f01f6b19ab7e93330e1606b9b2acb29f
alsgudrl132 <alsgudrl132@gmail.com>
2025-02-12 14:33:43 +0900

    타이틀 변경기능 추가

--- src/components/common/board.tsx
+++ src/components/common/board.tsx
@@ -1,5 +1,5 @@
-import { Plus, X } from "lucide-react";
-import React, { useState } from "react";
+import { Pencil, Plus, X } from "lucide-react";
+import React, { useEffect, useState } from "react";
 import Todo from "./todo";
 import { Button } from "../ui/button";
 import {
@@ -14,38 +14,81 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@radix-ui/react-label";
 
 interface BoardProps {
-  title: string;
   id: number;
+  title: string;
 }
 
 function Board({ title, id }: BoardProps) {
-  const [desc, setDesc] = useState<string>("");
+  const [isOpenDialog, setIsOpenDialog] = useState<boolean>(false);
+  const [isOpenBoardDialog, setIsOpenBoardDialog] = useState<boolean>(false);
+  const [description, setDescription] = useState<string>("");
+  const [newTitle, setNewTitle] = useState<string>("");
 
-  const createTodo = () => {
-    if (desc === "") return;
-    const storageData = JSON.parse(localStorage.getItem("boards") || "[]");
-    const idx = storageData.findIndex((board: BoardProps) => board.id === id);
-    if (!storageData[idx].desc) {
-      storageData[idx].desc = [desc];
+  const getStorageData = () => {
+    return JSON.parse(localStorage.getItem("boards") || "[]");
+  };
+
+  const getIdx = (storageData: BoardProps[]) => {
+    return storageData.findIndex((board: BoardProps) => board.id === id);
+  };
+
+  const createTodo = (e: object) => {
+    if (description === "") return;
+    if (e && "key" in e && e.key !== "Enter") return;
+    const storageData = getStorageData();
+    const idx = getIdx(storageData);
+    if (!storageData[idx].description) {
+      storageData[idx].description = [description];
     } else {
-      storageData[idx].desc.push(desc);
+      storageData[idx].description.push(description);
     }
     localStorage.setItem("boards", JSON.stringify(storageData));
-    setDesc("");
+    setDescription("");
+    setIsOpenDialog(false);
   };
+
+  const editTitle = () => {
+    setIsOpenBoardDialog(true);
+    const storageData = getStorageData();
+    const idx = getIdx(storageData);
+    setNewTitle(storageData[idx].title);
+  };
+
+  const updateTitle = (e: object) => {
+    if (newTitle === "") return;
+    if (e && "key" in e && e.key !== "Enter") return;
+
+    const storageData = getStorageData();
+    const idx = getIdx(storageData);
+    storageData[idx].title = newTitle;
+    localStorage.setItem("boards", JSON.stringify(storageData));
+    setIsOpenBoardDialog(false);
+  };
+
+  useEffect(() => {
+    setNewTitle(title);
+  }, [title]);
 
   return (
     <div className="mt-10 p-6 w-auto bg-white border border-gray-200 rounded-lg shadow-sm">
       <div className="flex items-center justify-between p-3 rounded-lg mb-4">
-        <p className="text-gray-800 font-medium">{title}</p>
-        <X
-          size={16}
-          className="text-gray-400 hover:text-red-500 cursor-pointer transition-colors"
-        />
+        <p className="text-gray-800 font-medium">{newTitle}</p>
+        <div className="flex gap-2">
+          <Pencil
+            size={16}
+            className="text-gray-400 hover:text-blue-500 cursor-pointer transition-colors"
+            onClick={() => editTitle()}
+          />
+          <X
+            size={16}
+            className="text-gray-400 hover:text-red-500 cursor-pointer transition-colors"
+          />
+        </div>
       </div>
+      <div className="border-b-2 mb-5"></div>
       <Todo id={id} />
       <div>
-        <Dialog>
+        <Dialog open={isOpenDialog} onOpenChange={setIsOpenDialog}>
           <DialogTrigger asChild>
             <Button
               variant="outline"
@@ -67,7 +110,8 @@ function Board({ title, id }: BoardProps) {
                   id="name"
                   placeholder="새 Todo 이름"
                   className="col-span-3"
-                  onChange={(e) => setDesc(e.target.value)}
+                  onChange={(e) => setDescription(e.target.value)}
+                  onKeyDown={(e) => createTodo(e)}
                 />
               </div>
             </div>
@@ -78,6 +122,37 @@ function Board({ title, id }: BoardProps) {
                 onClick={createTodo}
               >
                 추가하기
+              </Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+        <Dialog open={isOpenBoardDialog} onOpenChange={setIsOpenBoardDialog}>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>타이틀 변경</DialogTitle>
+            </DialogHeader>
+            <div className="grid gap-4 py-4">
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="name" className="text-right">
+                  타이틀
+                </Label>
+                <Input
+                  id="name"
+                  placeholder="새 타이틀 이름"
+                  className="col-span-3"
+                  value={newTitle}
+                  onChange={(e) => setNewTitle(e.target.value)}
+                  onKeyDown={(e) => updateTitle(e)}
+                />
+              </div>
+            </div>
+            <DialogFooter>
+              <Button
+                className="bg-blue-500 hover:bg-blue-600 text-white"
+                type="submit"
+                onClick={updateTitle}
+              >
+                변경하기
               </Button>
             </DialogFooter>
           </DialogContent>
--- src/components/common/todo.tsx
+++ src/components/common/todo.tsx
@@ -1,12 +1,22 @@
+import { Pencil, X } from "lucide-react";
+
 interface TodoProps {
   id: number;
+}
+
+interface BoardType {
+  id: number;
+  title: string;
+  description?: string[];
 }
 
 function Todo({ id }: TodoProps) {
   const getTodos = () => {
     const storageData = JSON.parse(localStorage.getItem("boards") || "[]");
-    const currentBoard = storageData.find((board: any) => board.id === id);
-    return currentBoard?.desc || [];
+    const currentBoard = storageData.find(
+      (board: BoardType) => board.id === id
+    );
+    return currentBoard?.description || [];
   };
 
   return (
@@ -14,9 +24,19 @@ function Todo({ id }: TodoProps) {
       {getTodos().map((todo: string, index: number) => (
         <div
           key={index}
-          className="flex items-center justify-between p-3 rounded-lg mb-3 bg-gray-50 hover:bg-gray-100 transition-colors"
+          className="flex gap-2 p-3 rounded-lg mb-3 bg-gray-50 hover:bg-gray-100 transition-colors w-52"
         >
-          <p className="text-gray-700">{todo}</p>
+          <p className="text-gray-700 text-sm break-all w-40">{todo}</p>
+          <div className="flex gap-2">
+            <Pencil
+              size={16}
+              className="text-gray-400 hover:text-blue-500 cursor-pointer transition-colors"
+            />
+            <X
+              size={16}
+              className="text-gray-400 hover:text-red-500 cursor-pointer transition-colors ml-auto"
+            />
+          </div>
         </div>
       ))}
     </div>
